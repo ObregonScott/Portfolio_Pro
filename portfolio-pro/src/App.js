1e84f111f959106a3ecff89ccbed71434e0850e1
obregonscott <obregon.scott@gmail.com>
2022-07-16 13:30:29 -0500

    stripped bootstrap, I hated it< starting over smh

---
+++ portfolio-pro/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import '../src/app.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Pickles
+      </h1>
+    </div>
+
+  );
+}
+
+export default App;
