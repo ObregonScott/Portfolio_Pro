9eee01ea3091482f312733ac327934f85edb8fd2
obregonscott <obregon.scott@gmail.com>
2022-07-17 13:44:41 -0500

    added NEW setup for portfolio

--- portfolio-pro/src/App.js
+++ portfolio-pro/src/App.js
@@ -1,14 +1,20 @@
-// import logo from './logo.svg';
-import '../src/app.css';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact';
+import './app.css';
 
 function App() {
   return (
-    <div>
-      <h1>
-        Pickles
-      </h1>
-    </div>
-
+    <main className='text-grey-400 bg-gray-900 body-font'>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
   );
 }
 
