c7a589bf3588d0ee8037a9be07dde1be29eb3079
obregonscott <obregon.scott@gmail.com>
2022-07-17 14:49:20 -0500

    redoing everything AGAIN

--- portfolio-pro/src/components/Navbar/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-  return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#107e57]'> Navbar </div>
-  )
-}
-
-export default Navbar;
