de05d13a70e4dd0737e073c2b736beec0896b98c
obregonscott <obregon.scott@gmail.com>
2022-07-17 18:22:56 -0500

    added garamond font from google api

--- portfolio-pro/src/components/Navbar.jsx
+++ portfolio-pro/src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {FaBars, FaTimes} from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/vulture.png';
 
 
 const Navbar = () => {
     return (
-        <div className='fixed w-full h-[80px] bg-[#011307] flex justify-between items-center px-4 text-gray-300'>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4  bg-[#011307]  text-gray-300'>
             <div>
-                <img src={Logo} alt="Logo Image" style={{width:'200px'}} />
+                <img src={Logo} alt="Logo Image" style={{ width: '200px' }} />
             </div>
-            <div>
-                <ul>
+
+            {/*Menu*/}
+            <div >
+                <ul className='flex'>
                     <li>Home</li>
                     <li>About</li>
                     <li>Skills</li>
@@ -18,6 +20,23 @@ const Navbar = () => {
                     <li>Contact</li>
                 </ul>
             </div>
+            {/* Hamburger */}
+            <div className='hidden'>
+                <FaBars />
+            </div>
+            {/* Mobile Menu */}
+            <ul className='hidden'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+            {/* Social Icons */}
+            <div className='hidden'>
+
+            </div>
+
         </div>
     )
 }
