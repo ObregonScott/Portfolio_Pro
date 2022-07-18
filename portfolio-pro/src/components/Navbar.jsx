5803bda601321608f9a6bc509a04dbf14f659049
obregonscott <obregon.scott@gmail.com>
2022-07-18 09:47:57 -0500

    ~added useState to open and close nav for mobile

--- portfolio-pro/src/components/Navbar.jsx
+++ portfolio-pro/src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/vulture.png';
 
 const Navbar = () => {
+        const [nav, setNav] = useState(false)
+        const handleClick = () => setNav(!nav)
+
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4  bg-[#011307]  text-gray-300'>
             <div>
@@ -21,11 +24,11 @@ const Navbar = () => {
                 </ul>
         
             {/* Hamburger */}
-            <div className='md:hidden z-10'>
-                <FaBars />
+            <div onClick={handleClick} className='md:hidden z-10'>
+                {!nav ? <FaBars/> : <FaTimes/>}
             </div>
             {/* Mobile Menu */}
-            <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#011307] flex flex-col justify-center items-center'>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#011307] flex flex-col justify-center items-center'} >
                 <li className='py-6 text-2xl'>Home</li>
                 <li className='py-6 text-2xl'>About</li>
                 <li className='py-6 text-2xl'>Skills</li>
