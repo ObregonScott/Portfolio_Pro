321ad2b05f33ce233cc160d82e19ea6ab0bd5b44
obregonscott <obregon.scott@gmail.com>
2022-07-18 08:43:08 -0500

    useState for nav

--- portfolio-pro/src/components/Navbar.jsx
+++ portfolio-pro/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/vulture.png';
-
 
 const Navbar = () => {
     return (
@@ -11,26 +11,26 @@ const Navbar = () => {
             </div>
 
             {/*Menu*/}
-            <div >
-                <ul className='flex'>
+           
+                <ul className='hidden md:flex'>
                     <li>Home</li>
                     <li>About</li>
                     <li>Skills</li>
                     <li>Projects</li>
                     <li>Contact</li>
                 </ul>
-            </div>
+        
             {/* Hamburger */}
-            <div className='hidden'>
+            <div className='md:hidden z-10'>
                 <FaBars />
             </div>
             {/* Mobile Menu */}
-            <ul className='hidden'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
+            <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#011307] flex flex-col justify-center items-center'>
+                <li className='py-6 text-2xl'>Home</li>
+                <li className='py-6 text-2xl'>About</li>
+                <li className='py-6 text-2xl'>Skills</li>
+                <li className='py-6 text-2xl'>Projects</li>
+                <li className='py-6 text-2xl'>Contact</li>
             </ul>
             {/* Social Icons */}
             <div className='hidden'>
