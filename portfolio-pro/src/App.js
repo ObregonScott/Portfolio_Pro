86cd1744fe3bf198e0d2898a3c5c1cfa0007b95d
Scott Obregon <obregon.scott@gmail.com>
2022-07-27 13:32:49 -0500

    added about, working on about

--- portfolio-pro/src/App.js
+++ portfolio-pro/src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Social from './components/Social'
+import About from './components/About'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
-
+      <About />
       <Social/>
     </div>
   )
