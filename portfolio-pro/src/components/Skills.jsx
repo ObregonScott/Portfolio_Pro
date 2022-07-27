b452480c8a3d3ab92457e00dab017da5e935d516
Scott Obregon <obregon.scott@gmail.com>
2022-07-27 16:03:43 -0500

    icons for skills

--- portfolio-pro/src/components/Skills.jsx
+++ portfolio-pro/src/components/Skills.jsx
@@ -1,29 +1,55 @@
 import React from 'react';
 import Html from '../assets/html.png';
-import Css from '../assets/css.png';
+import CSS from '../assets/css.png';
 import TailWind from '../assets/tailwind.png';
 import GitHub from '../assets/github.png';
 import Python from '../assets/python.png';
 import JavaScript from '../assets/javascript.png';
 import Node from '../assets/node.png';
-import Next from '../assets/nextjs.png';
-import MongoDb from '../assets/mongo.png';
 import ReactImg from '../assets/react.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-gradient-to-b from-black via-black to-gray-800 text-white'>
-      {/* Container */}
-      <div className='m-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div name='skills' className='bg-gradient-to-b from-black to-gray-800 text-white w-full h-screen'>
+    {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className=''>
-          <p className='text-4xl font-bold border-b-4 border-white'>Experience</p>
-          <p className='py-4'>//These are the technologies I've Worked with...</p>
+          <p className='text-4xl font-bold inline'>Skills</p>
+          <p className='py-4'>These are the technologies I Have Worked with...</p>
         </div>
-
-        <div className='w-full grid grid-col-2 sm:grid-cols-4 gap-4 text-cebnter py-8'>
-          <div>
+    {/* Icons */}
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-cebnter py-8'>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
             <img className='w-20 mx-auto' src={Html} alt="HTML icon" />
-            <p>HTML</p>
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+            <p className='my-4'>CSS</p>
+          </div>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
+            <img className='w-20 mx-auto' src={TailWind} alt="Tail Wind icon" />
+            <p className='my-4'>TailWind</p>
+          </div>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
+            <img className='w-20 mx-auto' src={GitHub} alt="Git Hub icon" />
+            <p className='my-4'>GitHub</p>
+          </div>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
+            <img className='w-20 mx-auto' src={Python} alt="Python icon" />
+            <p className='my-4'>Python</p>
+          </div>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
+            <img className='w-20 mx-auto' src={JavaScript} alt="Java Script icon" />
+            <p className='my-4'>JavaScript</p>
+          </div>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
+            <img className='w-20 mx-auto' src={Node} alt="Node icon" />
+            <p className='my-4'>Node</p>
+          </div>
+          <div className='shadow-md shadow-gray hover:scal-110 duration-500'>
+            <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
+            <p className='my-4'>React</p>
           </div>
         </div>
       </div>
