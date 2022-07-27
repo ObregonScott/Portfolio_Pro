bc55c93ff821e007e9243854e95a9af49aa25e87
Scott Obregon <obregon.scott@gmail.com>
2022-07-27 13:42:24 -0500

    working on about

--- portfolio-pro/src/components/About.jsx
+++ portfolio-pro/src/components/About.jsx
@@ -4,11 +4,11 @@ const About = () => {
   return (
     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 via-black to-black text-white'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div>
+        <div className='max-w-(1000px) w-full px-4 grid grid-cols-2 gap-8'>
           <p>About</p>
         </div>
         <div>
-          
+
         </div>
       </div>
     </div>
